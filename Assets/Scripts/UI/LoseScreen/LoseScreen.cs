39ac6e28b4170113561bb120d3000da4b54a15f8
Konstantin Osintsev <rojuty@gmail.com>
2021-12-23 14:02:45 +0700

    lose screen added

---
+++ Assets/Scripts/UI/LoseScreen/LoseScreen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreen : IWindow
+{
+    private readonly LoseScreenInitialData _initData;
+    private readonly LoseScreenUIData _uiData;
+    private readonly HighScore _highScore;
+
+    public LoseScreen(LoseScreenInitialData data)
+    {
+        _initData = data;
+        _uiData = _initData.Canvas.GetComponent<LoseScreenUIData>();
+        _highScore = new HighScore();
+        _initData.Hp.HpOver += Open;
+    }
+
+    public event Action Opened;
+    public event Action Closed;
+
+    public void Close()
+    {
+        _initData.Canvas.enabled = false;
+        Closed?.Invoke();
+    }
+
+    public void Open()
+    {
+        _initData.Canvas.enabled = true;
+        Opened?.Invoke();
+        UpdateInfo();
+        _uiData.Retry.onClick.AddListener(ReloadScene);
+    }
+
+    private void UpdateInfo()
+    {
+        _uiData.Lose.text = "You Lose!";
+        _uiData.CurrentScore.text = $"Your Score: {_initData.Score.CurrentScore}";
+        _uiData.BestScore.text = $"Best score: {_highScore.TryLoad()}";
+    }
+
+    private void ReloadScene() // for tests only
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
+
+public struct LoseScreenInitialData
+{
+    public Canvas Canvas;
+    public Score Score;
+    public Hp Hp;
+}
---
+++ Assets/Scripts/UI/LoseScreen/LoseScreenUIData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseScreenUIData : MonoBehaviour
+{
+    [SerializeField] private Text _youLose;
+    [SerializeField] private Text _currentScore;
+    [SerializeField] private Text _bestScore;
+    [SerializeField] private Button _retry;
+
+    public Text Lose => _youLose;
+    public Text CurrentScore => _currentScore;
+    public Text BestScore => _bestScore;
+    public Button Retry => _retry;
+    
+    
+}
